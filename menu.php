a354ed0a4a44a88a13861db797311d2ee4e6f836
Kush Khurana <kushkhurana0002@gmail.com>
2019-09-27 18:25:50 +0530

    menu changes

--- menu.php
+++ menu.php
@@ -5,11 +5,12 @@
 <li><a href="#">About Us</a></li>
 <li><a href="#">Services</a>
 	<div class="submenu">
-		<a href="#">Menu 1</a>
-		<a href="#">Menu 2</a>
-		<a href="#">Menu 3</a>
-		<a href="#">Menu 4</a>
-		<a href="#">Menu 5</a>
+		<a href="#">Menu</a>
+		<a href="#">Menu</a>
+		<a href="#">Menu</a>
+		<a href="#">Menu</a>
+		<a href="#">Menu</a>
+		<a href="#">Menu</a>
 	</div>
 </li>
 <li><a href="#">Enquiry</a></li>
