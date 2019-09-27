878247009a840508ba8db2a5e29cebd2868f931b
Kush Khurana <kushkhurana0002@gmail.com>
2019-09-27 17:37:36 +0530

    first level menu complete

---
+++ menu.php
@@ -0,0 +1,18 @@
+<link rel="stylesheet" href="css/menu.css" />
+
+<ul class="menu">
+<li><a href="#">Home</a></li>
+<li><a href="#">About Us</a></li>
+<li><a href="#">Services</a>
+	<!--<div class="submenu">
+		<a href="#">Menu 1</a>
+		<a href="#">Menu 2</a>
+		<a href="#">Menu 3</a>
+		<a href="#">Menu 4</a>
+		<a href="#">Menu 5</a>
+	</div>-->
+</li>
+<li><a href="#">Enquiry</a></li>
+<li><a href="#">Contact Us</a></li>
+
+</ul>
